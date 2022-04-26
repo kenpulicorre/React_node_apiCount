b2f9272cd0f517328d8d6560fb75edeca96e8f81
kenneth <ingkeneidel@gmail.com>
2022-04-26 17:17:19 -0500

    añadi la rutacon api ya pude llamar y variasr info

--- api/src/models/Country.js
+++ api/src/models/Country.js
@@ -6,25 +6,26 @@ module.exports = (sequelize) => {
   sequelize.define(
     "country",
     {
-      // id: {
-      //   type: DataTypes.STRING,
-      //   allowNull: false,
-      //   validate: { isAlpha: true, len: [3] },
-      //   primaryKey: true,
-      // },
       id: {
-        type: DataTypes.UUID,
-        defaultValue: DataTypes.UUIDV4,
+        type: DataTypes.STRING,
         allowNull: false,
+        validate: { isAlpha: true, len: [3] },
         primaryKey: true,
       },
+      // id: {
+      //   type: DataTypes.UUID,
+      //   defaultValue: DataTypes.UUIDV4,
+      //   allowNull: false,
+      //   primaryKey: true,
+      // },
       name: { type: DataTypes.STRING, allowNull: false },
-      img_flag: { type: DataTypes.STRING, allowNull: false },
+
       continent: { type: DataTypes.STRING, allowNull: false },
       capital: { type: DataTypes.STRING, allowNull: false },
       subregion: { type: DataTypes.STRING, allowNull: true },
       area: { type: DataTypes.FLOAT, allowNull: true },
       people: { type: DataTypes.STRING, allowNull: true },
+      img_flag: { type: DataTypes.STRING, allowNull: false },
     },
     { timestamps: false }
   );
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -4,18 +4,88 @@ const { Country, Activity } = require("../db");
 // Ejemplo: const authRouter = require('./auth.js');
 
 const router = Router();
+const axios = require("axios");
+const urlAll = `https://restcountries.com/v3/all`;
+const urlAllT = ` https://pokeapi.co/api/v2/type`;
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-router.get("/", (req, res, next) => {
-  res.send(`hola desde landingpage`);
+//---funciones
+//++++getapi info countries-----
+
+const getApiInfoCountries = async () => {
+  try {
+    const countryApi = await axios.get(urlAll); //para apinfo
+    const apiInfo = await countryApi.data.map((e) => {
+      let capital = e.capital ? e.capital[0] : "no info";
+      return {
+        id: e.cca3, //es como el id
+        name: e.translations.spa.common,
+        img_flag: e.flags[1],
+        continent: e.continents[0],
+        capital: capital,
+        subregion: e.subregion,
+        area: e.area,
+        people: e.population,
+      };
+    });
+    return apiInfo;
+    //---fin solo 20------
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const addToDbCountry = async () => {
+  const x = await getApiInfoCountries();
+  console.log(x[0]);
+  // return { img_flag, continent, capital, subregion, area, people };
+  const countryToDb = x.slice(0, 50).map(async (e) => {
+    await Country.create({
+      id: e.id,
+      name: e.name,
+      img_flag: e.img_flag,
+      continent: e.continent,
+      capital: e.capital,
+      subregion: e.subregion,
+      area: e.area,
+      people: e.people,
+    });
+  });
+
+  return { x };
+};
+//++++fin getapi info countries-----
+//---fin de funciones
+
+//definiendo rutas iniciales sin modulizar aun
+router.get("/", async (req, res, next) => {
+  const dbCountry = await addToDbCountry();
+  res.send("se llamo y creo en base de datos los paises");
 });
-router.get("/countries", (req, res, next) => {
+
+router.get("/countries", async (req, res, next) => {
   const { name } = req.query;
+
+  let countryDb = await Country.findAll({
+    order: [["name", "ASC"]],
+    attributes: { exclude: ["createdAt", "updatedAt"] },
+    include: {
+      model: Activity,
+      // attributes: ["name"],
+      // through: {
+      //   attributes: [],
+      // },
+    },
+    // attributes: ["name"],
+    // through: { attributes: [] },
+  });
+
   if (name) {
     res.send(`hola desde home con query busqueda por nombre pais: ${name}`);
   } else {
-    res.send(`hola desde home sin query`);
+    res.json(countryDb);
+    console.log(`hola desde home sin query get("/countries-->`);
   }
 });
 
@@ -45,15 +115,29 @@ router.post("/activity", async (req, res, next) => {
       season,
     });
 
+    // let newActivity2= Activity.findOrCreate({
+    //   where: {
+    //     name,
+    //     difficulty,
+    //     duration,
+    //     season,
+    //     url: e.url,
+    //   },
+    // });
+
     let countryDb = await Country.findAll({
       // where: { name: ["fire", "flying"] },
       where: { name: country },
       attributes: { exclude: ["createdAt", "updatedAt"] },
     });
+    console.log("countryDb----", countryDb.data);
     newActivity.addCountry(countryDb);
 
     res.send(newActivity);
   } catch (error) {
+    if (error.name === "SequelizeUniqueConstraintError") {
+      res.send("ya existe el nombre de la actividad");
+    }
     res.send(error);
   }
 });
